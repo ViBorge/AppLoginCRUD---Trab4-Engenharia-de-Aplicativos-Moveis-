5cf1f304b2d0c71f0d3db75b30968eb94fb09245
Vinicius Borges <vinicius.borgesouza@gmail.com>
2026-06-13 18:01:27 -0300

    deu tudo errado

---
+++ AppLoginCRUD/app/index.js
@@ -0,0 +1,5 @@
+import CrudExemploScreen from '../src/screens/CrudExemploScreen';
+
+export default function Index() {
+  return <CrudExemploScreen />;
+}
---
+++ AppLoginCRUD/src/App.js
@@ -0,0 +1,5 @@
+import CrudExemploScreen from '../../src/screens/CrudExemploScreen';
+
+export default function HomeScreen() {
+  return <CrudExemploScreen />;
+}   
