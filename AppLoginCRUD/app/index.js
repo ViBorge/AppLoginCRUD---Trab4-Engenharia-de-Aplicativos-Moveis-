8487df9ad62ab75131b3bef10843b672bf86c27c
Vinicius Borges <vinicius.borgesouza@gmail.com>
2026-06-18 22:03:03 -0300

    Mudando toda a estrututra

--- AppLoginCRUD/app/index.js
+++
@@ -1,5 +0,0 @@
-import CrudExemploScreen from '../src/screens/CrudExemploScreen';
-
-export default function Index() {
-  return <CrudExemploScreen />;
-}
--- AppLoginCRUD/src/App.js
+++
@@ -1,5 +0,0 @@
-import CrudExemploScreen from '../../src/screens/CrudExemploScreen';
-
-export default function HomeScreen() {
-  return <CrudExemploScreen />;
-}   
