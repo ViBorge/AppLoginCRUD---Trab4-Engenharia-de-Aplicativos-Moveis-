152d30c5dd7038f50cd5853882fbc710040b0aa7
Vinicius Borges <vinicius.borgesouza@gmail.com>
2026-06-20 14:31:47 -0300

    Correção de bug do firebase tela branca

--- AppLoginFirebase/app/(auth)/_layout.tsx
+++ AppLoginFirebase/app/(auth)/_layout.tsx
@@ -1,21 +1,11 @@
 import { Stack } from 'expo-router';
 import React from 'react';
-import { AuthProvider } from '../src/contexts/AuthContext'; 
 
-export default function RootLayout() {
+export default function AuthLayout() {
   return (
-    <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        
-        {/* Telas Públicas (Área deslogada) */}
-        <Stack.Screen name="index" options={{ title: 'Login' }} />
-        <Stack.Screen name="cadastro" options={{ title: 'Criar Conta' }} />
-        
-        {/* Telas Protegidas (Grupo de rotas da pasta auth) */}
-        {/* É estritamente necessário registrar o grupo aqui para que o replace funcione */}
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        
-      </Stack>
-    </AuthProvider>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="perfil" />
+      <Stack.Screen name="editar" />
+    </Stack>
   );
 }
